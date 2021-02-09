ca3fb4703e7b887c8a947c3b4afa80c0163d9e42
daniapm <daniajibellis@hotmail.com>
2021-02-09 14:00:39 +0000

    Write a function that prints the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,24 +1,19 @@
-#include <unistd.h>
-#include <stdio.h>
+#include "holberton.h"
+
 
 /**
- * main - check the code for Holberton School students.
- * Return: Always 0.
- * print_alphabet - Función donde se ejecuta la lógica del programa.
- *
+ * print_alphabet - prints every minute of the day
+ * main - Return
+ * Return: void
  */
 void print_alphabet(void)
 {
 int letras;
 for (letras = 97; letras <= 122; letras++)
 {
-putchar(letras); /* imprime la cadena */
+_putchar(letras);
 }
-putchar('\n');
-return; /* sale de la función */
+_putchar('\n');
+return;
 }
-int main(void)
-{
-print_alphabet();
-return (0);
-}
+
