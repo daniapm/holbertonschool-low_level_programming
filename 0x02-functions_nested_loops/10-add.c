1beae8128c09fa90d5909051c66013e5fd19b3ae
daniapm <daniajibellis@hotmail.com>
2021-02-09 16:32:18 +0000

    Write a function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * add - checks for lowercase letters
+ * @numero1: variable
+ * @numero2: variable 2
+ * main - Return
+ *
+ * Return: 0 or 1
+ */
+
+int add(int numero1, int numero2)
+{
+int suma;
+suma = numero1 + numero2;
+return (suma);
+}
