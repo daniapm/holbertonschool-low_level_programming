766a22642ad013244dd0be6153a3330087f4541e
daniapm <daniajibellis@hotmail.com>
2021-02-10 00:10:53 +0000

    Fix the code in 2-largest_number.c so that it correctly prints out the largest of three numbers, no matter the case.0

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,18 +12,17 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && a > c)
+if (a >= b && a >= c)
 {
 largest = a;
 }
-else if (b > c && b > a)
+else if (b >= c && b >= a)
 {
 largest = b;
 }
-else if (c > b && c > a)
+else
 {
 largest = c;
 }
-
 return (0);
 }
