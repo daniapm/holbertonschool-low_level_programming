c4c4c51ebc3e01494b58094e96b15eace286a2bd
daniapm <daniajibellis@hotmail.com>
2021-02-10 22:59:31 +0000

    comment

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,13 +12,13 @@ int i;
 printf("Infinite loop incoming :(\n");
 /**
  * i = 0; - dont delete it!
- *
+ * printf("Infinite loop avoided! \\o/\n");
  * while (i < 10)
  * {
  * putchar(i);
  * }
- *
- * printf("Infinite loop avoided! \\o/\n"); - parametro
-*/
+ */
+printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
