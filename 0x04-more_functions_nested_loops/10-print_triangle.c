e0ac121c7aee088d3c7802f16811642fa61d46f0
daniapm <daniajibellis@hotmail.com>
2021-02-12 04:10:35 +0000

    Write a function that prints a triangle, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - print triangle
+ * @size: variable
+ * main - Return
+ *
+ * Return: 0 or 1
+ */
+
+void print_triangle(int size)
+{
+int contador, numeral;
+if (size == 0)
+{
+_putchar (10);
+}
+for (contador = 0; contador < size; contador++)
+{
+for (numeral = size - 1; numeral >= 0; numeral--)
+{
+if (numeral > contador)
+{
+_putchar (32);
+}
+else
+{
+_putchar (35);
+}
+}
+_putchar(10);
+}
+return;
+}
