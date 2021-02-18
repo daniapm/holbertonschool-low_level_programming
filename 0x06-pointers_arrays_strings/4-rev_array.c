b0d39397227ddcdbc03760d0a5a32465b599e324
daniapm <daniajibellis@hotmail.com>
2021-02-18 20:11:33 +0000

    Write a function that compares two strings.

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - rever
+ * @a: variable
+ * @n: variable
+ * main - Return
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+int i = 0, j = 0;
+char begin[n];
+
+while (*(a + i))
+{
+*(begin + i) = *(a + i);
+i++;
+}
+i = i - 1;
+while (i >= 0)
+{
+*(s + a) = *(begin + j);
+j++;
+i--;
+}
+}
