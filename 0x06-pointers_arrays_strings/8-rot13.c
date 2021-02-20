76c04586413a5cb2443fa098ac73db0e2ccf32f2
daniapm <daniajibellis@hotmail.com>
2021-02-20 04:27:28 +0000

    Write a function that encodes a string using rot13.

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ * *rot13 - Segmentation
+ * @s: variable
+ * main - Return
+ * Return: void
+ */
+char *rot13(char *s)
+{
+char abc[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char root[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+int i, j;
+i = 0;
+j = 0;
+ 
+for (i = 0; s[i]; i++)
+{
+for (j = 0; j < 52; j++)
+{
+if (abc[j] == s[i])
+{
+s[i] = root[j];
+break;
+}
+}
+}
+return (s);
+}
