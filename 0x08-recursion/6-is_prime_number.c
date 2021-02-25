4193aec5191e79f162477491e5e50171a8c7f070
daniapm <daniajibellis@hotmail.com>
2021-02-25 02:28:39 +0000

    Write a function that returns the natural square root of a number.

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+
+/**
+ * factorial - print to strings.
+ * @n: variable
+ * main - Return
+ * Return: void
+ */
+
+int is_prime_number(int n)
+{
+if (i >= 0)
+{
+i++;
+}
+if (n % 1 == 0)
+{
+ 
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -9,5 +9,8 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int resultado(int n, int a);
+int is_prime_number(int n);
 
 #endif /* HOLBERTON_H */
