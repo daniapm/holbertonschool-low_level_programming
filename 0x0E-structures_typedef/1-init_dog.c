2bc894c45eebef40c0bd74a8bf235e88d977d988
daniapm <daniajibellis@hotmail.com>
2021-03-08 21:35:24 +0000

    Write a function that prints a struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "dog.h"
-
+#include <stdlib.h>
 /**
  * init_dog - prototype
  * @d: varianle 1
@@ -12,6 +12,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
