63629871db61ac5bbdcdff8bd664035724b67b84
daniapm <daniajibellis@hotmail.com>
2021-03-09 01:44:03 +0000

    Write a function that initialize a variable of type struct dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
+char *_strcpy(char *dest, char *src);
+int largo(char *s);
 
 /**
  * *new_dog - Entry Point
@@ -17,10 +19,64 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(struct dog));
 	if (dog == NULL)
 	{
+		free(dog);
 		return (NULL);
 	}
-	dog->name = name;
+	dog->name = malloc(sizeof(char) * (largo(name) + 1));
+	if (dog->name ==  NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->name = _strcpy(dog->name, name);
 	dog->age = age;
-	dog->owner = owner;
+
+	dog->owner = malloc(sizeof(char) * (largo(owner) + 1));
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = _strcpy(dog->owner, owner);
+
+
 	return (dog);
+}
+
+/**
+ * *_strcpy - copy
+ * @dest: variable
+ * @src: variable
+ *return: 0
+ *
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *puntero = dest;
+	while (*src != '\0')
+	{
+		*puntero = *src;
+		puntero++;
+		src++;
+	}
+	*puntero = '\0';
+	return (dest);
+}
+/**
+ * largo - largo
+ * @s: variable
+ * main - Return
+ * Return: void
+ *
+ */
+int largo(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
