3edfc0ab19b09a020a0977416419291cde9ca554
daniapm <daniajibellis@hotmail.com>
2021-03-19 19:57:25 +0000

    Write a function that frees a list_t list.

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,11 +11,13 @@
 
 void free_list(list_t *head)
 {
-	list_t *puntero;
+	list_t *puntero = head;
 
 	while (head != NULL)
+	{
 		puntero = head->next;
 	free(head->str);
 	free(head);
 	head = puntero;
+	}
 }
