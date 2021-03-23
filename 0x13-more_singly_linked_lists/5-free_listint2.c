8b897425607375e4e998667fd2ef03dd3d62e0a8
daniapm <daniajibellis@hotmail.com>
2021-03-23 16:49:25 +0000

    file is corrected

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,8 +14,8 @@ void free_listint2(listint_t **head)
 
 	listint_t *curren;
 
-	if (head != NULL || *head != NULL)
-	{
+	if (head == NULL || *head == NULL)
+	return;
 	while (*head != NULL)
 	{
 		curren = *head;
@@ -23,5 +23,5 @@ void free_listint2(listint_t **head)
 		free(curren);
 	}
 	*head = NULL;
-	}
 }
+
