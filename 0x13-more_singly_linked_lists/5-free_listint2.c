154141451178d73e996697d10d1267f2bb372600
daniapm <daniajibellis@hotmail.com>
2021-03-23 16:45:32 +0000

    file is corrected

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,15 +12,14 @@
 void free_listint2(listint_t **head)
 {
 
-listint_t *current;
-if (head != NULL && *head != NULL)
-{
-while (head != NULL)
+	listint_t *curren;
+
+	if (head != NULL || *head != NULL)
+	while (*head != NULL)
 	{
-current = *head;
-*head = (*head)->next;
-free(current);
+		curren = *head;
+		*head = (*(head))->next;
+		free(curren);
 	}
-}
 	*head = NULL;
 }
