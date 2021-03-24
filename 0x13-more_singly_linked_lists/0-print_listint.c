2ebf2a00163df5a081c3f0d134159b17c3cb70fb
daniapm <daniajibellis@hotmail.com>
2021-03-24 03:06:30 +0000

    changes are updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -15,7 +15,7 @@ size_t print_listint(const listint_t *h)
 
 	i = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 	printf("%d\n", h->n);
 	h = h->next;
--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -4,30 +4,29 @@
 
 
 /**
- * *insert_nodeint_at_index - Entry Point
+ * delete_nodeint_at_index - Entry Point
  * @head: variable
- * @idx: variable
- * @n: variable
+ * @index: variable
  * Return: 0
  */
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 
-listint_t *temp, sig;
+listint_t *temp, *sig;
 unsigned int i;
-    if (index == 0)
-    {
-    temp = (*(head))->next;
-        free(temp);
-    }
-    for (i = 0; temp != NULL && i < index - 1; i++)
-         temp = temp->next;
-    if (temp == NULL || temp->next == NULL)
-         return(-1);
- 	sig = *temp->next->next;
-    free(temp->next);
 
-    *temp->next = sig;
+if (index == 0)
+{
+temp = (*head)->next;
+*head = temp;
 return (1);
+}
+for (i = 0; temp != NULL && i < index - 1; i++)
+temp = temp->next;
+if (temp == NULL || temp->next == NULL)
+return (-1);
+sig = temp->next->next;
+*temp->next = *sig;
+return (-1);
 }
