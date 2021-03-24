ab61b6e3f7208c24cef5c5565b0b2ff65c925084
daniapm <daniajibellis@hotmail.com>
2021-03-24 02:41:57 +0000

    changes are updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,12 +10,16 @@
 
 size_t print_listint(const listint_t *h)
 {
+
 	size_t i;
 
-	for (i = 0; h; i++)
+	i = 0;
+
+	while (h != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+	printf("%d\n", h->n);
+	h = h->next;
+	i++;
 	}
 	return (i);
 }
