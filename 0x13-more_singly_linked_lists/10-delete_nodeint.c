e53b0a4e3b7fa006e87d02e6e857c9860d4a8738
daniapm <daniajibellis@hotmail.com>
2021-03-25 01:35:34 +0000

    changes are updated

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,29 +14,29 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 
 unsigned int ctr = 0;
-	listint_t *temp_h, *temp_node;
+	listint_t *temp_h, *nuevo_node;
 
-	if (!*head) /* EMPTY list */
+	if (!*head)
 		return (-1);
 	temp_h = *head;
-	if (index == 0)
+	if (index != 0)
 	{
-		temp_node = temp_h->next;
-		*head = temp_node;
-		free(temp_h);
-		return (1);
-	}
 	while (temp_h)
 	{
-		if (ctr + 1 == index)
+		if (ctr < index)
 		{
-			temp_node = temp_h->next;
-			temp_h->next = temp_node->next;
-			free(temp_node);
+			nuevo_node = temp_h->next;
+			temp_h->next = nuevo_node->next;
+			free(nuevo_node);
 			return (1);
 		}
 		temp_h = temp_h->next;
 		ctr++;
 	}
 	return (-1);
+	}
+	nuevo_node = temp_h->next;
+		*head = nuevo_node;
+		free(temp_h);
+		return (1);
 }
