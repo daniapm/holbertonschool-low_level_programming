719e6756f48ddc9a51f198e96c6ea140b1742eef
daniapm <daniajibellis@hotmail.com>
2021-03-26 00:52:44 +0000

    changes are updated

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,5 +13,11 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-return ((n & (1 << index)) > 0);
+unsigned int b;
+
+b = ((n & (1 << index)) > 0);
+if (b == 1 || b == 0)
+return (b);
+else
+return (-1);
 }
