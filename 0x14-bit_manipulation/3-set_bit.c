3e8d9d2f518ae609b3b9a5b9ce08412a18cbd160
daniapm <daniajibellis@hotmail.com>
2021-03-26 03:40:40 +0000

    changes are updated

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -13,8 +13,12 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > (sizeof(unsigned long int) * 8 - 1))
-		return (-1);
+unsigned int mask;
 
-return (*n | 1 << index);
+if (index > (sizeof(unsigned long int) * 8 - 1))
+return (-1);
+mask = (1 << index);
+mask = (1 << index);
+*n = mask | *n;
+return (1);
 }
