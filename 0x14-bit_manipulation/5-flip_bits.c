64886101c8a8bd9fa82319ec88e27f3e042676a3
daniapm <daniajibellis@hotmail.com>
2021-03-27 03:04:55 +0000

    changes are updated

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "holberton.h"
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * flip_bits - Entry Point
+ * @n: variable
+ * @m: variable
+ * Return: 0
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+int distance = 0;
+unsigned int val;
+val = n ^ m;
+
+if (distance <= 32)
+while (val)
+{
+distance++;
+val &= val - 1;
+}
+return (distance);
+}
