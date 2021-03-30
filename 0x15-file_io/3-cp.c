73a4a9488bbb215740a0f9c9b87400f0d77988d4
vagrant <vagrant@vagrant.vm>
2021-03-30 21:59:57 +0000

    se actualizan cambios

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,3 @@
-
-unsigned int _strlen(char *s);
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -23,31 +21,37 @@ char buffer[1024];
 
 if (argc < 3)
 {
-exit(99);
 printf("Usage: cp file_from file_to\n");
+exit(97);
 }
 file_from = open(argv[1], O_RDONLY);
-file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0700);
-if (file_from == -1 || file_to == 1)
+if (file_from == -1)
 {
-printf("Error: Can't read from file NAME_OF_THE_FILE");
+printf("Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-fd = read(file_from, buffer, 1024);
-if (fd > 0)
+file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0700);
+if (file_to == -1)
 {
-imprimir = write(file_to, buffer, fd);
-}
-if (imprimir != fd)
-{
-printf("Error: Can't write to NAME_OF_THE_FILE");
+printf("Error: Can't read from file %s\n", argv[2]);
 exit(99);
 }
+while (fd >= 1024)
+{
+fd = read(file_from, buffer, 1024);
 if (fd == -1)
 {
-printf("problem in reading");
-exit(2);
+printf("Error: Can't read from file %s\n", argv[1]);
+exit(98);
 }
+imprimir = write(file_to, buffer, fd);
+if (imprimir == -1)
+{
+printf("Error: Can't read from file %s\n", argv[2]);
+exit(99);
+}
+}
+close(file_from);
 close(file_to);
-exit(0);
+return (0);
 }
