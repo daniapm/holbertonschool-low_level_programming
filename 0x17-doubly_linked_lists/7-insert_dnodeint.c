887bdf0cba8fd63b5b0dfba793e033def494b1c4
daniapm <daniajibellis@hotmail.com>
2021-05-03 02:34:19 +0000

    inserts a new node at a given position.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,36 +13,33 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-unsigned int i = 0;
-	dlistint_t *new_node = malloc(sizeof(dlistint_t)), *tmp_node = *h;
+dlistint_t *nuevo_nodo, *tmp;
 
-	if (new_node)
+if (h == NULL)
+return (NULL);
+nuevo_nodo = malloc(sizeof(dlistint_t));
+if (nuevo_nodo == NULL)
+{
+return (NULL);
+}
+nuevo_nodo->n = n;
+if (idx == 0)
+{
+nuevo_nodo->next = *h;
+*h = nuevo_nodo;
+return (nuevo_nodo);
+}
+tmp = *h;
+while (idx - 1 > 0)
 	{
-		new_node->n = n;
-		if (idx == 0)
-		{
-			if (*h)
-			{
-				new_node->next = *h;
-				(*h)->prev = new_node;
-			}
-			*h = new_node;
-			return (new_node);
-		}
-		while (i < idx - 1 && tmp_node)
-		{
-			tmp_node = tmp_node->next;
-			i++;
-		}
-		if (tmp_node)
-		{
-			if (tmp_node->next)
-				tmp_node->next->prev = new_node;
-			new_node->next = tmp_node->next;
-			new_node->prev = tmp_node;
-			tmp_node->next = new_node;
-			return (new_node);
-		}
-	}
+tmp = tmp->next;
+if (tmp == NULL)
+{
 	return (NULL);
+}
+idx--;
+}
+nuevo_nodo->next = tmp->next;
+tmp->next = nuevo_nodo;
+return (nuevo_nodo);
 }
