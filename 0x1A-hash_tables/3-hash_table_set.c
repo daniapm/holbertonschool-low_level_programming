25a9864609e8b5926293d49485f2873bc2e658ab
Dania Puertas <daniajibellis@hotmail.com>
2021-06-04 20:14:47 +0000

    corrigiendo errores de collision

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,36 +12,44 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int count;
 	hash_node_t *hash_node;
-	char *new_value;
 
 	count = key_index((const unsigned char *)key, ht->size);
 	if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
 		return (0);
 
-	while (ht->array[count] != NULL)
-	{
-
-		if (strcmp(ht->array[count]->key, key) == 0)
-		{
-			new_value = strdup(value);
-			if (new_value == NULL)
-			{
-				return (0);
-			}
-			free(ht->array[count]->value);
-			ht->array[count]->value = new_value;
-			return (1);
-		}
-		ht->array[count] = ht->array[count]->next;
-	}
 	hash_node = create_items(key, value);
 	if (hash_node == NULL)
 	{
 		return (0);
 	}
-	hash_node->next = ht->array[count];
-	ht->array[count] = hash_node;
-	return (1);
+	if (ht->array[count] != NULL)
+	{
+		while (ht->array[count] != NULL)
+		{
+			if (strcmp(ht->array[count]->key, key) == 0)
+			{
+				break;
+			}
+			ht->array[count] = ht->array[count]->next;
+		}
+		if (ht->array[count] == NULL)
+		{
+			hash_node->next = ht->array[count];
+			ht->array[count] = hash_node;
+		}
+		else
+		{
+			free(ht->array[count]->value);
+			ht->array[count]->value = strdup(value);
+		}
+
+	}
+	else
+	{
+		hash_node->next = NULL;
+		ht->array[count] = hash_node;
+	}
+	return (0);
 }
 /**
  * create_items - function that adds an element to the hash table.
